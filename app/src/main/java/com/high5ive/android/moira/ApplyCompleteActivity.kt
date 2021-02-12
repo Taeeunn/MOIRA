096167480f90558bbb4899e4c10e828c52b16788
taeeunn <xodms8713@gmail.com>
2021-02-13 02:43:39 +0900

    feat: some layout

---
+++ app/src/main/java/com/high5ive/android/moira/ApplyCompleteActivity.kt
@@ -0,0 +1,11 @@
+package com.high5ive.android.moira
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplyCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apply_complete)
+    }
+}
