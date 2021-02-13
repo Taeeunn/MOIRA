acc6acebe825e53d69f61cd56d591ebd3d63f60f
taeeunn <xodms8713@gmail.com>
2021-02-14 00:05:43 +0900

    feat: 공모전 목록

---
+++ app/src/main/java/com/high5ive/android/moira/ContestListActivity.kt
@@ -0,0 +1,11 @@
+package com.high5ive.android.moira
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContestListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contest_list)
+    }
+}
