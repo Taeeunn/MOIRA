08bea19ec5112605ca1655ef5c13592e245f20c7
taeeunn <xodms8713@gmail.com>
2021-02-13 22:48:27 +0900

    feat: transparent toolbar

--- app/src/main/java/com/high5ive/android/moira/FindContestActivity.kt
+++ app/src/main/java/com/high5ive/android/moira/FindContestActivity.kt
@@ -2,10 +2,29 @@ package com.high5ive.android.moira
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class FindContestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_contest)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val ab = supportActionBar!!
+        ab.setDisplayShowTitleEnabled(false)
+        ab.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/high5ive/android/moira/LoginActivity.kt
+++ app/src/main/java/com/high5ive/android/moira/LoginActivity.kt
@@ -2,10 +2,19 @@ package com.high5ive.android.moira
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+
+
     }
+
+
 }
