74fabca6c575116de2bff29a41d29de0ed63860c
taeeunn <xodms8713@gmail.com>
2021-02-22 13:55:17 +0900

    feat: team detail page

---
+++ app/src/main/java/com/high5ive/android/moira/Recruit.kt
@@ -0,0 +1,6 @@
+package com.high5ive.android.moira
+
+data class Recruit(
+    val title: String,
+    val nickname: String
+)
---
+++ app/src/main/java/com/high5ive/android/moira/TeamDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.high5ive.android.moira
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeamDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_detail)
+    }
+}
