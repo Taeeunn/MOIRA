0cb0eaef3d97bb8d85bc173278d7ffb1b9476f36
taeeunn <xodms8713@gmail.com>
2021-02-26 20:16:55 +0900

    feat: team item

--- app/src/main/java/com/high5ive/android/moira/InProgressFragment.kt
+++ app/src/main/java/com/high5ive/android/moira/InProgressFragment.kt
@@ -34,7 +34,7 @@ class InProgressFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_in_progress, container, false)
+        return inflater.inflate(R.layout.in_progress_fragment, container, false)
     }
 
     companion object {
