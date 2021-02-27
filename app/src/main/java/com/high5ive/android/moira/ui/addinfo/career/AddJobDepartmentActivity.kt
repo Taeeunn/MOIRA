8e502eca3051f81c17208e58e8b259c931f66fa3
taeeunn <xodms8713@gmail.com>
2021-02-28 06:53:45 +0900

    feat: add job department page

---
+++ app/src/main/java/com/high5ive/android/moira/ui/addinfo/career/AddJobDepartmentActivity.kt
@@ -0,0 +1,12 @@
+package com.high5ive.android.moira.ui.addinfo.career
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.high5ive.android.moira.R
+
+class AddJobDepartmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_job_department)
+    }
+}
