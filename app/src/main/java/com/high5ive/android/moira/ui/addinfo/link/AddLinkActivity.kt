f4e6c94ef26e8666dbb8845cad1d453885b09b67
taeeunn <xodms8713@gmail.com>
2021-02-28 07:02:57 +0900

    feat: add link page

---
+++ app/src/main/java/com/high5ive/android/moira/ui/addinfo/link/AddLinkActivity.kt
@@ -0,0 +1,12 @@
+package com.high5ive.android.moira.ui.addinfo.link
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.high5ive.android.moira.R
+
+class AddLinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_link)
+    }
+}
