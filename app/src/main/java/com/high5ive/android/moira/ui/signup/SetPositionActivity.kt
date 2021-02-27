54989023f20a994848f5c47eec9e872d24da7581
taeeunn <xodms8713@gmail.com>
2021-02-28 01:19:35 +0900

    feat: position 설정 page

---
+++ app/src/main/java/com/high5ive/android/moira/ui/signup/SetPositionActivity.kt
@@ -0,0 +1,12 @@
+package com.high5ive.android.moira.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.high5ive.android.moira.R
+
+class SetPositionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_position)
+    }
+}
