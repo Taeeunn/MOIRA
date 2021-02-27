65a99b044976af9124844d56bb9a432234200948
taeeunn <xodms8713@gmail.com>
2021-02-28 07:22:42 +0900

    feat: add award history page

---
+++ app/src/main/java/com/high5ive/android/moira/ui/addinfo/award/AddAwardHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.high5ive.android.moira.ui.addinfo.award
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.high5ive.android.moira.R
+
+class AddAwardHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_award_history)
+    }
+}
