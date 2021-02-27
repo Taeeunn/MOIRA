b259a193fd663bbb9e39a0968c1d8c3afdf7f5fc
taeeunn <xodms8713@gmail.com>
2021-02-28 02:27:14 +0900

    feat: 관심 태그 설정 page

---
+++ app/src/main/java/com/high5ive/android/moira/ui/signup/SetTagActivity.kt
@@ -0,0 +1,12 @@
+package com.high5ive.android.moira.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.high5ive.android.moira.R
+
+class SetTagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_tag)
+    }
+}
