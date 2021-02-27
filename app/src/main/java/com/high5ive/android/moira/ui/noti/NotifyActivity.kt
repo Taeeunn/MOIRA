3d192d58610deff380e777d123d5eb3754f11c2c
taeeunn <xodms8713@gmail.com>
2021-02-28 07:50:51 +0900

    feat: add notification page

---
+++ app/src/main/java/com/high5ive/android/moira/ui/noti/NotifyActivity.kt
@@ -0,0 +1,23 @@
+package com.high5ive.android.moira.ui.noti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.high5ive.android.moira.R
+import com.high5ive.android.moira.adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.my_team_fragment.*
+
+class NotifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notify)
+
+        viewPager.adapter = ViewPagerAdapter(this)
+        val tabLayoutTextArray = arrayOf("지원 소식","받은 초대")
+        TabLayoutMediator(tabLayout,viewPager){tab,position->
+            tab.text = tabLayoutTextArray[position]
+
+        }.attach()
+    }
+}
