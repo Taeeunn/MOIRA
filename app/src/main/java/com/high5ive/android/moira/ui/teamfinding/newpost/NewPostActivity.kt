9c84536d1c9e14140e9cb397c4eae5359435625c
taeeunn <xodms8713@gmail.com>
2021-03-01 07:25:40 +0900

    refactor: new post page

--- app/src/main/java/com/high5ive/android/moira/ui/teamfinding/newpost/NewPostActivity.kt
+++ app/src/main/java/com/high5ive/android/moira/ui/teamfinding/newpost/NewPostActivity.kt
@@ -1,14 +1,15 @@
-package com.high5ive.android.moira
+package com.high5ive.android.moira.ui.teamfinding.newpost
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import com.high5ive.android.moira.R
 
-class MakeTeamActivity : AppCompatActivity() {
+class NewPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_make_team)
+        setContentView(R.layout.activity_new_post)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
