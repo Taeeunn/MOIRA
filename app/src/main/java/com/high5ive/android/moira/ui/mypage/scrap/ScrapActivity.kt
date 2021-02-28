941a8131c2045c049a4bf3d6686dd036103ff9bd
taeeunn <xodms8713@gmail.com>
2021-02-28 09:38:12 +0900

    feat: scrap page

---
+++ app/src/main/java/com/high5ive/android/moira/ui/mypage/scrap/ScrapActivity.kt
@@ -0,0 +1,23 @@
+package com.high5ive.android.moira.ui.mypage.scrap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.high5ive.android.moira.R
+import com.high5ive.android.moira.adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.my_team_fragment.*
+
+class ScrapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrap)
+
+        viewPager.adapter = ViewPagerAdapter(this)
+        val tabLayoutTextArray = arrayOf("모집글","인재풀")
+        TabLayoutMediator(tabLayout,viewPager){tab,position->
+            tab.text = tabLayoutTextArray[position]
+
+        }.attach()
+    }
+}
