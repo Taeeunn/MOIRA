96591fc6e33deb92571fc444ae32865730798162
taeeunn <xodms8713@gmail.com>
2021-03-01 18:38:33 +0900

    feat: apply page

--- app/src/main/java/com/high5ive/android/moira/ui/teamfinding/apply/ApplyActivity.kt
+++ app/src/main/java/com/high5ive/android/moira/ui/teamfinding/apply/ApplyActivity.kt
@@ -1,12 +1,12 @@
-package com.high5ive.android.moira.ui.apply
+package com.high5ive.android.moira.ui.teamfinding.apply
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.high5ive.android.moira.R
 
-class ApplyCompleteActivity : AppCompatActivity() {
+class ApplyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_apply_complete)
+        setContentView(R.layout.activity_apply)
     }
 }
