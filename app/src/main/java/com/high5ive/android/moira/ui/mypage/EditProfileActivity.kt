1b4a6804b5b362db2d41ab45bdf1531f2939e3e8
taeeunn <xodms8713@gmail.com>
2021-03-01 19:04:28 +0900

    refactor: apply page

---
+++ app/src/main/java/com/high5ive/android/moira/ui/mypage/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.high5ive.android.moira.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.high5ive.android.moira.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
