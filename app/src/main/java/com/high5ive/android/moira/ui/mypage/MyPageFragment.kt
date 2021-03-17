db5c74317fe931031e748d9daac62079dd0fe7bd
taeeunn <xodms8713@gmail.com>
2021-03-18 02:03:30 +0900

    feat: Ask Activity

--- app/src/main/java/com/high5ive/android/moira/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/high5ive/android/moira/ui/mypage/MyPageFragment.kt
@@ -9,12 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.high5ive.android.moira.R
 import com.high5ive.android.moira.ui.mypage.apply.ApplyListActivity
+import com.high5ive.android.moira.ui.mypage.cs.AskActivity
 import com.high5ive.android.moira.ui.mypage.edit.EditProfileActivity
 import com.high5ive.android.moira.ui.mypage.post.PostListActivity
 import com.high5ive.android.moira.ui.mypage.scrap.ScrapListActivity
 import kotlinx.android.synthetic.main.my_page_fragment.*
 
-class MyPageFragment : Fragment() {
+class MyPageFragment : Fragment(), View.OnClickListener{
 
     companion object {
         fun newInstance() = MyPageFragment()
@@ -34,20 +35,30 @@ class MyPageFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MyPageViewModel::class.java)
         // TODO: Use the ViewModel
 
-        post_container.setOnClickListener {
-            startActivity(Intent(context, PostListActivity::class.java))
-        }
+        post_container.setOnClickListener(this)
 
-        apply_container.setOnClickListener {
-            startActivity(Intent(context, ApplyListActivity::class.java))
-        }
+        apply_container.setOnClickListener(this)
 
-        scrap_container.setOnClickListener {
-            startActivity(Intent(context, ScrapListActivity::class.java))
-        }
+        scrap_container.setOnClickListener(this)
 
-        edit_info_btn.setOnClickListener {
-            startActivity(Intent(context, EditProfileActivity::class.java))
+        edit_info_btn.setOnClickListener(this)
+
+        account_setting.setOnClickListener(this)
+
+        noti_setting.setOnClickListener(this)
+
+        question.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.post_container -> startActivity(Intent(context, PostListActivity::class.java))
+            R.id.apply_container -> startActivity(Intent(context, ApplyListActivity::class.java))
+            R.id.scrap_container -> startActivity(Intent(context, ScrapListActivity::class.java))
+            R.id.edit_info_btn -> startActivity(Intent(context, EditProfileActivity::class.java))
+            R.id.account_setting -> startActivity(Intent(context, EditProfileActivity::class.java))
+            R.id.noti_setting -> startActivity(Intent(context, EditProfileActivity::class.java))
+            R.id.question -> startActivity(Intent(context, AskActivity::class.java))
         }
     }
 
---
+++ app/src/main/java/com/high5ive/android/moira/ui/mypage/cs/AskActivity.kt
@@ -0,0 +1,36 @@
+package com.high5ive.android.moira.ui.mypage.cs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.high5ive.android.moira.R
+import kotlinx.android.synthetic.main.activity_ask.*
+
+class AskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ask)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val ab = supportActionBar!!
+        ab.setDisplayShowTitleEnabled(false)
+        ab.setDisplayHomeAsUpEnabled(true)
+
+        register_button.setOnClickListener{
+            finish()
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
